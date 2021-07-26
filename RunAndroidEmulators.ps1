6af1ae797bc72d4a03c8df655845fbf9704f7f38
Rodolfo Fagundes <rodolfo.fagundes.f@gmail.com>
2021-07-26 19:57:43 -0300

    Update RunAndroidEmulators.ps1

--- RunAndroidEmulators.ps1
+++ RunAndroidEmulators.ps1
@@ -1,14 +1,15 @@
 # ***** Select file *****
 
-$files = Get-ChildItem *.ini -Path C:\Users\DEV-RODOLFO\.android\avd
+$files = Get-ChildItem *.ini -Path C:\Users\Rodolfo\.android\avd
 
 $title = ''
 $subtitle = 'Choose a emulator'
 
-$fileChoices = @()
+$fileChoices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
 
 for ($i = 0; $i -lt $files.Count; $i++) {
-    $fileChoices += [System.Management.Automation.Host.ChoiceDescription]("$($files[$i].Name) &$($i + 1)")
+    #$fileChoices += [System.Management.Automation.Host.ChoiceDescription[]]("$($files[$i].Name) &$($i + 1)")
+    $fileChoices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList $files[$i].Name))
 }
 
 $userChoice = $host.UI.PromptForChoice($title, $subtitle, $fileChoices, 0)
@@ -18,22 +19,24 @@ Write-Host "You choose $nameChoice"
 
 # ***** Delete snapshots *****
 
-$pathExist = Test-Path -Path "C:\Users\DEV-RODOLFO\.android\avd\$nameChoice.avd\snapshots"
+$pathExist = Test-Path -Path "C:\Users\Rodolfo\.android\avd\$nameChoice.avd\snapshots"
 
 if ($pathExist) {
-    $title = ''
-    $subtitle = 'Do you want to delete snapshots?'
-    $choices = '&Yes', '&No'
+    #$title = ''
+    #$subtitle = 'Do you want to delete snapshots?'
+    #$choices = '&Yes', '&No'
     
-    $decision = $Host.UI.PromptForChoice($title, $subtitle, $choices, 1)
-    if ($decision -eq 0) {
-        Remove-Item -Path "C:\Users\DEV-RODOLFO\.android\avd\$nameChoice.avd\snapshots" -Recurse
-    }
+    #$decision = $Host.UI.PromptForChoice($title, $subtitle, $choices, 1)
+    #if ($decision -eq 0) {
+    #    Remove-Item -Path "C:\Users\Rodolfo\.android\avd\$nameChoice.avd\snapshots" -Recurse
+    #}
+
+    Remove-Item -Path "C:\Users\Rodolfo\.android\avd\$nameChoice.avd\snapshots" -Recurse
 }
 
 # ***** Wipe data *****
 
-$fileExist = Test-Path -Path "C:\Users\DEV-RODOLFO\.android\avd\$nameChoice.avd\userdata-qemu.img" -PathType Leaf
+$fileExist = Test-Path -Path "C:\Users\Rodolfo\.android\avd\$nameChoice.avd\userdata-qemu.img" -PathType Leaf
 
 if ($fileExist) {
     $title = ''
@@ -42,8 +45,8 @@ if ($fileExist) {
     
     $decision = $Host.UI.PromptForChoice($title, $subtitle, $choices, 1)
     if ($decision -eq 0) {
-        Remove-Item -Path "C:\Users\DEV-RODOLFO\.android\avd\$nameChoice.avd\userdata-qemu.img" -Force
+        Remove-Item -Path "C:\Users\Rodolfo\.android\avd\$nameChoice.avd\userdata-qemu.img" -Force
     }
 }
 
-C:\Users\DEV-RODOLFO\AppData\Local\Android\Sdk\emulator\emulator.exe -avd $nameChoice
+C:\Users\Rodolfo\AppData\Local\Android\Sdk\emulator\emulator.exe -avd $nameChoice
